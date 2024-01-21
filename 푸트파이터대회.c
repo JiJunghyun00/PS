26450e43187f773fe58b5eedb1ac94a185692a5c
jh-ji <mju19jhj@gmail.com>
2024-01-21 21:06:56 +0900

    240121

--- 푸트파이터대회.c
+++ 푸트파이터대회.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
+#include <string.h>
 // food_len은 배열 food의 길이입니다.
 char* solution(int food[], size_t food_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
@@ -15,6 +15,16 @@ char* solution(int food[], size_t food_len) {
     for (int i = 1;i < food_len;i++) {
         answerlen += food[i];
     }
-    //미완성
+    int i = 0;
+    int j = 0;
+    int check = 0;
+    for (i = 1;i < food_len;i++) {
+        for (j = check;j < (check + (food[i] / 2));j++) {
+            answer[j] = (i + '0');
+        }
+        check = j;
+    }
+    //check++;
+    //answer[check]='0';
     return answer;
 }
