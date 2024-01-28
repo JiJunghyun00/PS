27663da4aa9fe90f42abfcf17fa8417d6088f67e
jh-ji <mju19jhj@gmail.com>
2024-01-28 11:09:46 +0900

    240128

--- 명예의전당1.c
+++ 명예의전당1.c
@@ -7,11 +7,18 @@ int* solution(int k, int score[], size_t score_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * score_len);
 
-    int* temp = answer;
+    int* temp = (int*)malloc(sizeof(int) * score_len);
+    int* tempOriginAddress = temp;
+    temp[0] = score[0];
+    temp[1] = score[1];
+    temp[2] = score[2];
     for (int i = 0;i < score_len;i++) {
-
+        for (int j = 0;j <= i;j++) {
+            score[]
+        }
         *temp = score[i];
         temp++;
+
     }
     return answer;
 }
