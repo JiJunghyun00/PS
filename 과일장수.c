055a9754343c6cbdd2837645b91a0e5616f98d92
jh-ji <mju19jhj@gmail.com>
2024-02-03 15:04:59 +0900

    240203

---
+++ 과일장수.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// score_len은 배열 score의 길이입니다.
+int solution(int k, int m, int score[], size_t score_len) {
+    int answer = 0;
+    for (int i = 0;i < score_len;i++) {
+
+    }
+    return answer;
+}
