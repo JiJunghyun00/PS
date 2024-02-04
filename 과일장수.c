3294ab56d50ceb752f67de8be78d9de83b3a1325
jh-ji <mju19jhj@gmail.com>
2024-02-04 23:16:16 +0900

    240204

--- 과일장수.c
+++ 과일장수.c
@@ -5,8 +5,15 @@
 // score_len은 배열 score의 길이입니다.
 int solution(int k, int m, int score[], size_t score_len) {
     int answer = 0;
-    for (int i = 0;i < score_len;i++) {
+    int check = 0;
+    int max = 0;
 
+    for (int i = 0;i < score_len;i++) {
+        if (score[i] > max) {
+            max = score[i];
+            check = i;
+        }
     }
+    score[check] = 0;
     return answer;
 }
