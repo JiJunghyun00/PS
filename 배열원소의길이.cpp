950f7a687f43e4a561e699dc0a889296febb4b24
jh-ji <mju19jhj@gmail.com>
2024-02-25 19:52:57 +0900

    240225

---
+++ 배열원소의길이.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    answer.resize(strlist.size());
+    for (int i = 0;i < strlist.size();i++) {
+        answer[i] = strlist[i].size();
+    }
+    return answer;
+}
